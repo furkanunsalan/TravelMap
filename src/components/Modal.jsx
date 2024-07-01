90212aa591428e5424f8cfef9221038098036a86
Furkan Ünsalan <furkanunsalan@gmail.com>
2024-07-01 18:44:23 +0300

    added modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const Modal = ({ onClose, children }) => {
-    return (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-            <div className="fixed inset-0 bg-gray-800 opacity-75"></div>
-            <div className="bg-white p-6 rounded-lg shadow-lg z-50 max-w-lg">
-                <button onClick={onClose} className="absolute top-0 right-0 p-2 m-2 text-gray-500 hover:text-gray-700">
-                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="fixed inset-0 bg-gray-800 opacity-75"></div>
+      <div className="bg-white p-6 rounded-lg shadow-lg z-50 max-w-lg">
+        <button
+          onClick={onClose}
+          className="absolute top-0 right-0 p-2 m-2 text-gray-500 hover:text-gray-700"
+        >
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
